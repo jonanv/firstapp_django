e263fb175817ebd857b284d8161dddc3e4e1a08b
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-05 19:02:40 -0500

    Se agrega metodo que es llamado por la ruta en url.py y es devuelto en html como vista

--- miapp/views.py
+++ miapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Modelo Vista Controlador -> Acciones (metodos)
+# MVT = Modelo Template Vista -> Acciones (metodos)
+
+def holaMundo(request):
+    return HttpResponse("""
+        <h1>
+            Hola mundo con Django!
+        </h1>
+    """)
