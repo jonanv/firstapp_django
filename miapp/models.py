9737677f1d86310c9135056bb6e4ffecebb6250c
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-11 18:26:38 -0500

    Se modifica el modelo, agregando un nuevo campo y modificando un campo de Article y en Category se modifica un campo, posterior se ejecuta python3 manage.py makemigrations, python3 manage.py sqlmigrate miapp 0002 y python3 manage.py migrate

--- miapp/models.py
+++ miapp/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 150)
     content = models.TextField()
+    image = models.ImageField(default = 'null')
     public = models.BooleanField()
     created_at = models.DateField(auto_now_add = True)
     upgrade_at = models.DateField(auto_now = True)
 
 class Category(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 110)
     description = models.CharField(max_length = 250)
     created_at = models.DateField()
