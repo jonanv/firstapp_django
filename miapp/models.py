5d0c9bab8aa5669b9427a65590b863ff2a13f602
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-11 10:41:13 -0500

    Se agrega modelo de Article en el archivo models.py

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    public = models.BooleanField()
+    created_at = models.DateField(auto_now_add = True)
+    upgrade_at = models.DateField(auto_now = True)
+
+class Category(models.Model):
+    
