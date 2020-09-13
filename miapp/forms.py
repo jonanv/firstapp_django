be29d686c6f8e7f0949316312025b3710c7ce8db
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-13 12:11:57 -0500

    Campo tipo select desde el formulario utilizando una clase

--- miapp/forms.py
+++ miapp/forms.py
@@ -4,6 +4,16 @@ class FormArticle(forms.Form):
     title = forms.CharField(
         label = "Titulo"
     )
+
     content = forms.CharField(
         label = "Contenido"
+    )
+
+    public_options = [
+        (1, 'Si'),
+        (0, 'No')
+    ]
+    public = forms.TypedChoiceField(
+        label = "Publicado",
+        choices = public_options
     )
