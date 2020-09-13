70ff1c1f2e6bb656bc857a3ab8de42f075f054c5
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-13 15:24:32 -0500

    Se agregan propiedades al modelo para las clases Article y Category

--- miapp/models.py
+++ miapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length = 150)
-    content = models.TextField()
-    image = models.ImageField(default = 'null')
-    public = models.BooleanField()
-    created_at = models.DateTimeField(auto_now_add = True)
-    upgrade_at = models.DateTimeField(auto_now = True)
+    title = models.CharField(max_length=150, verbose_name='Título')
+    content = models.TextField(verbose_name='Contenido')
+    image = models.ImageField(default='null', verbose_name='Imagen')
+    public = models.BooleanField(verbose_name='Público')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upgrade_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Artículo"
@@ -15,8 +15,8 @@ class Article(models.Model):
         ordering = ['-id']
 
 class Category(models.Model):
-    name = models.CharField(max_length = 110)
-    description = models.CharField(max_length = 250)
+    name = models.CharField(max_length=110, verbose_name='Nombre')
+    description = models.CharField(max_length=250, verbose_name='Descripción')
     created_at = models.DateTimeField()
 
     class Meta:
