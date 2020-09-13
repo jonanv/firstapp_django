8e6e22ce32021a85a400085b02bf62eb3edd4cea
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-13 13:15:48 -0500

    Personalizar formulario desde la clase

--- miapp/forms.py
+++ miapp/forms.py
@@ -2,11 +2,27 @@ from django import forms
 
 class FormArticle(forms.Form):
     title = forms.CharField(
-        label = "Titulo"
+        label = "Titulo",
+        max_length = 20,
+        required = True,
+        widget = forms.TextInput(
+            attrs = {
+                'placeholder': 'Ingresa el titulo',
+                'class': 'form-control'
+            }
+        )
     )
 
     content = forms.CharField(
-        label = "Contenido"
+        label = "Contenido",
+        max_length = 100,
+        required = True,
+        widget = forms.Textarea(
+            attrs = {
+                'placeholder': 'Ingresa el contenido',
+                'class': 'form-control'
+            }
+        )
     )
 
     public_options = [
